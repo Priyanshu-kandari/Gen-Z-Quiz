f231c6cc224bfb32d8e270fd1a5df53c336f321a
Priyanshu <Preyanshukandari@gmail.com>
2025-11-22 20:33:13 +0530

    Add Gen Z quiz project

---
+++ app.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("#quizform")
+const resultText = document.getElementById("result-text")
+const resultTag = document.getElementById("result-Tag")
+
+
+const correct = {
+        q1: "KR$NA & Seedhe Maut",
+        q2: "Charisma/flirting skill",
+        q3: "Rajkumar Rao",
+        q4: "acha ji aisa hai kya",
+        q5: "Panchayat"
+    };
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    let score = 0
+
+    for(let keys in correct){
+        const selected = document.querySelector(`input[name = ${keys}]:checked`)
+        if(selected && selected.value == correct[keys]){
+            score++
+        }
+    }
+
+    resultText.innerText = `you scored ${score}`
+
+    if(score==4){
+        resultTag.innerText = "Certified Gen Z Legend 🏆🔥"
+    }else if(score>=3){
+        resultTag.innerText = "Pretty updated ngl 😎"
+    }else{
+        resultTag.innerText = "Bro update your pop culture knowledge 💀"
+    }
+})
